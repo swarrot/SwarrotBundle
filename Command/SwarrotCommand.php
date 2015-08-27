23919e5efb4c8c153a0f1c7f05edb5492a11d1fa
metfan <l_ulrich2002@yahoo.fr>
2015-08-27 15:55:44 +0200

    use extra from config.yml as default value for options

--- Command/SwarrotCommand.php
+++ Command/SwarrotCommand.php
@@ -59,13 +59,28 @@ class SwarrotCommand extends ContainerAwareCommand
         ;
 
         if (array_key_exists('ack', $this->processorStack)) {
-            $this->addOption('requeue-on-error', 'r', InputOption::VALUE_NONE, 'Requeue in the same queue on error');
+            $this->addOption(
+                'requeue-on-error',
+                'r',
+                InputOption::VALUE_OPTIONAL,
+                'Requeue in the same queue on error',
+                (isset($this->extras['requeue_on_error']))?$this->extras['requeue_on_error']:false);
         }
         if (array_key_exists('max_execution_time', $this->processorStack)) {
-            $this->addOption('max-execution-time', 't', InputOption::VALUE_REQUIRED, 'Max execution time (seconds) before exit', 300);
+            $this->addOption(
+                'max-execution-time',
+                't',
+                InputOption::VALUE_REQUIRED,
+                'Max execution time (seconds) before exit',
+                (isset($this->extras['max_execution_time']))?$this->extras['max_execution_time']:300);
         }
         if (array_key_exists('max_messages', $this->processorStack)) {
-            $this->addOption('max-messages', 'm', InputOption::VALUE_REQUIRED, 'Max messages to process before exit', 300);
+            $this->addOption(
+                'max-messages',
+                'm',
+                InputOption::VALUE_REQUIRED,
+                'Max messages to process before exit',
+                (isset($this->extras['max_messages']))?$this->extras['max_messages']:300);
         }
         if (array_key_exists('exception_catcher', $this->processorStack)) {
             $this->addOption('no-catch', 'C', InputOption::VALUE_NONE, 'Deactivate exception catching.');
@@ -75,7 +90,12 @@ class SwarrotCommand extends ContainerAwareCommand
         }
         if (array_key_exists('retry', $this->processorStack)) {
             $this->addOption('no-retry', 'R', InputOption::VALUE_NONE, 'Deactivate retry.');
-            $this->addOption('retry-attempts', null, InputOption::VALUE_REQUIRED, 'Number of maximum retry attempts (if it exists, override the extra data parameter)');
+            $this->addOption(
+                'retry-attempts',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Number of maximum retry attempts (if it exists, override the extra data parameter)',
+                (isset($this->extras['retry_attempts']))?$this->extras['retry_attempts']:3);
         }
     }
 
@@ -166,11 +186,7 @@ class SwarrotCommand extends ContainerAwareCommand
             $options['retry_key_pattern'] = $key;
 
             $attempts = 3;
-            if (isset($this->extras['retry_attempts'])) {
-                $attempts = $this->extras['retry_attempts'];
-            }
-
-            if (null !== $input->getOption('retry-attempts')) {
+            if ($input->hasOption('retry-attempts')) {
                 $attempts = (int) $input->getOption('retry-attempts');
             }
             $options['retry_attempts'] = $attempts;
